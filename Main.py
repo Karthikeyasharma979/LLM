e09270c05534b3f2c93bc2686eece4c26a2e56c9
Karthikeyasharma979 <90711330+Karthikeyasharma979@users.noreply.github.com>
2024-04-21 10:53:20 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,19 +4,15 @@ import transformers
 import torch
 from transformers import pipeline
 import os
-
-
-
-def ai(res):
-  model = "microsoft/phi-2" # meta-llama/Llama-2-7b-hf
-  tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
-  llama_pipeline = pipeline(
-    "text-generation",  # LLM task
-    model=model,
-    torch_dtype=torch.float32,
-    device_map="auto",
-  )
-  return llama_pipeline(res)
+model = "microsoft/phi-2" # meta-llama/Llama-2-7b-hf
+tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
+llama_pipeline = pipeline(
+  "text-generation",  # LLM task
+  model=model,
+  torch_dtype=torch.float32,
+  device_map="auto",
+)
+llama_pipeline(res)
   
 st.title("Phi-2")
 col1,col2 = st.columns(2)
@@ -26,5 +22,5 @@ os.environ["HUGGINGFACEHUB_API_TOKEN"]="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"
 os.environ["HF_TOKEN"]="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"
 res = st.button("Submit")
 if res :
-    str = ai(ques)
+    str = llama_pipeline(res)
     st.write(str)

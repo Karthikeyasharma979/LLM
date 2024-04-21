bbbcda0f39e3553acd7a49a17f95047c8be2f55f
Karthikeyasharma979 <90711330+Karthikeyasharma979@users.noreply.github.com>
2024-04-21 10:56:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,6 +4,8 @@ import transformers
 import torch
 from transformers import pipeline
 import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"]="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"
+os.environ["HF_TOKEN"]="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"
 model = "microsoft/phi-2" # meta-llama/Llama-2-7b-hf
 tokenizer = AutoTokenizer.from_pretrained(model, use_auth_token=True)
 llama_pipeline = pipeline(
@@ -18,8 +20,6 @@ st.title("Phi-2")
 col1,col2 = st.columns(2)
 ques = col1.text_input("Enter the Query : ")
 key = col2.text_input("Enter the Key : ")
-os.environ["HUGGINGFACEHUB_API_TOKEN"]="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"
-os.environ["HF_TOKEN"]="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"
 res = st.button("Submit")
 if res :
     str = llama_pipeline(res)
